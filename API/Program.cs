0da50bbc50d9545b60a386311e09acc96f3a7669
unggoths <hellgangboy@gmail.com>
2025-03-27 18:08:43 +0200

    111

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,16 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 DbInitializer.InitDb(app);
 
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 app.MapControllers();
 
 app.Run();
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [products, setProducts] = useState<{ name: string, price: number }[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        fetch('http://localhost:5045/api/product')
+            .then(response => response.json())
+            .then(data => setProducts(data));
+
+    }, []);
+
+    const addProduct = () => {
+        setProducts(prevState => [...prevState, { name: 'product ' + (prevState.length + 1), price: 300.00 }]);
+    }
+
+    return (
+        <div>
+            <h1>Re-Store</h1>
+            <ul>
+                {products.map((item, index) => (
+                    <li key={index}>{item.name} - {item.price}</li>
+                ))}
+            </ul>
+            <button onClick={addProduct}>Add Product</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
